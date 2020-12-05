7d175d929cd7feeccb0a2234dedc441f095d67ec
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 11:15:38 +0800

    save

--- todo_drf/urls.py
+++ todo_drf/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from todo_drf.api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+
+
