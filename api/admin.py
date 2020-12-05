f7f2f6a4ece4f2fd46c6f05da094ca76112ff6d9
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 14:31:30 +0800

    save

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
+from .models import Task, Article
 
 # Register your models here.
-
-from .models import Task
-
 admin.site.register(Task)
 admin.site.register(Article)
