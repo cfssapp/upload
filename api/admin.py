3045e83fc1af4938a0c1b4a16c3cdbb6fc181164
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 14:19:42 +0800

    save

--- api/admin.py
+++ api/admin.py
@@ -5,3 +5,4 @@ from django.contrib import admin
 from .models import Task
 
 admin.site.register(Task)
+admin.site.register(Article)
--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,30 @@
 from rest_framework import serializers
-from .models import Task
-from django.contrib.auth.models import User, Group
+from .models import Task, Article
+
 
 class TaskSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Task
 		fields ='__all__'
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+class ArticleSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    author = serializers.CharField(max_length=100)
+    email = serializers.EmailField(max_length=100)
+    date = serializers.DateTimeField()
+ 
+    def create(self, validated_data):
+ 
+        # Create and return a new `Article` instance, given the validated data.
+        return Article.objects.create(validated_data)
+ 
+    def update(self, instance, validated_data):
+ 
+        #Update and return an existing `Article` instance, given the validated data.
+ 
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.email = validated_data.get('email', instance.email)
+        instance.date = validated_data.get('date', instance.date)
+        instance.save()
+        return instance
