29f3471e14f194c41a8a4da20ca290ca59a09c36
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 14:53:22 +0800

    save

--- api/views.py
+++ api/views.py
@@ -1,17 +1,11 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from .serializers import UserSerializer, GroupSerializer
-
-
 from django.shortcuts import render
-from django.http import JsonResponse
-
+from django.http import JsonResponse, HttpResponse
+from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import TaskSerializer
+from .serializers import TaskSerializer, ArticleSerializer
 
-from .models import Task
+from .models import Task, Article
 # Create your views here.
 
 @api_view(['GET'])
@@ -67,19 +61,21 @@ def taskDelete(request, pk):
 	return Response('Item succsesfully delete!')
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
+@csrf_exempt
+def article_list(request):
+    """
+    List all code articles, or create a new Article.
+    """
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
