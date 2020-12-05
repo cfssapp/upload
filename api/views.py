ee5c1b8324e81b16a79a87fee5d58b51a1e6ac91
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 16:26:49 +0800

    save

--- api/views.py
+++ api/views.py
@@ -774,13 +774,15 @@ jsonData = [
 
 @api_view(['GET'])
 def apiOverview(request):
-	api_urls = {
-		'List':'/task-list/',
-		'Detail View':'/task-detail/<str:pk>/',
-		'Create':'/task-create/',
-		'Update':'/task-update/<str:pk>/',
-		'Delete':'/task-delete/<str:pk>/',
-		}
+	# api_urls = {
+	# 	'List':'/task-list/',
+	# 	'Detail View':'/task-detail/<str:pk>/',
+	# 	'Create':'/task-create/',
+	# 	'Update':'/task-update/<str:pk>/',
+	# 	'Delete':'/task-delete/<str:pk>/',
+	# 	}
+
+	api_urls = jsonData	
 
 	return JsonResponse(api_urls)
 
