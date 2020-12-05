748c86b15c9c43cce40fe09315f581e6175bbdc4
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 10:13:53 +0800

    save

--- api/urls.py
+++ api/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
 	path('task-list/', views.taskList, name="task-list"),
 	path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
 	path('task-create/', views.taskCreate, name="task-create"),
-
 	path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
 	path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),
+
+	path('users', views.UserViewSet, name="user-view"),
+	path('groups', views.GroupViewSet, name="group-view"),
 ]
 
 
