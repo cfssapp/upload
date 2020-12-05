87161d4f9b2cc3763b5a0343771fa1115fb5c5dc
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 16:41:46 +0800

    save

--- api/views.py
+++ api/views.py
@@ -782,7 +782,47 @@ def apiOverview(request):
 	# 	'Update':'/task-update/<str:pk>/',
 	# 	'Delete':'/task-delete/<str:pk>/',
 	# 	}
-	api_urls = [{"id": 1, "title": "New Article", "author": "Parwiz"}]
+
+	# api_urls = [{"id": 1, "title": "New Article", "author": "Parwiz"}]
+
+	api_urls = [{
+        "id": "fake-list-0",
+        "owner": "付小小",
+        "title": "Alipay",
+        "avatar": "https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png",
+        "cover": "https://gw.alipayobjects.com/zos/rmsportal/uMfMFlvUuceEyPpotzlq.png",
+        "status": "active",
+        "percent": 83,
+        "logo": "https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png",
+        "href": "https://ant.design",
+        "updatedAt": 1607155074056,
+        "createdAt": 1607155074056,
+        "subDescription": "那是一种内在的东西， 他们到达不了，也无法触及的",
+        "description": "在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。",
+        "activeUser": 144426,
+        "newUser": 1421,
+        "star": 148,
+        "like": 194,
+        "message": 17,
+        "content": "段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。",
+        "members": [
+            {
+                "avatar": "https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png",
+                "name": "曲丽丽",
+                "id": "member1"
+            },
+            {
+                "avatar": "https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png",
+                "name": "王昭君",
+                "id": "member2"
+            },
+            {
+                "avatar": "https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png",
+                "name": "董娜娜",
+                "id": "member3"
+            }
+        ]
+    }]
 	return JsonResponse(api_urls, safe=False)
 
 @api_view(['GET'])
