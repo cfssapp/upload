ddf9dbac3b40a764fd0d49d4c61cbc6cdef5333a
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 21:39:57 +0800

    save

--- api/views.py
+++ api/views.py
@@ -16,18 +16,17 @@ from .models import Task, Article
 def apiOverview(request):
 	api_urls = [{
         "id": "fake-list-0",
-        "owner": "付小小",
-        "title": "Alipay",
-        "avatar": "https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png",
-        "cover": "https://gw.alipayobjects.com/zos/rmsportal/uMfMFlvUuceEyPpotzlq.png",
-        "status": "active",
-        "percent": 83,
+
         "logo": "https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png",
+        "title": "Alipay",
         "href": "https://ant.design",
-        "updatedAt": 1607155074056,
-        "createdAt": 1607155074056,
         "subDescription": "那是一种内在的东西， 他们到达不了，也无法触及的",
-        "description": "在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。",
+        
+        "owner": "付小小",
+        "createdAt": 1607155074056,
+        "updatedAt": 1607155074056,
+        "percent": 83,
+        "status": "active", 
     }]
 	return JsonResponse(api_urls, safe=False)
 
