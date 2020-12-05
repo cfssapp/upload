78f2ae46e10dc0eeebca990ded57f9ad2aac437f
Lowcostdiy <gohweihan21@gmail.com>
2020-12-05 15:04:33 +0800

    save

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
