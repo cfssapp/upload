dd36d45ebb52e2e14a249aa95efedadb1418e789
Lowcostdiy <gohweihan21@gmail.com>
2020-12-06 11:00:18 +0800

    save

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://drfserver.pythonanywhere.com/media/'
 
 CORS_ORIGIN_WHITELIST = [
     "https://www.moba.my",
