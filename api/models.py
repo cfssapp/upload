58e7f2471a67c91190de8bdaf874e1c5a13d93c0
cfssapp <gohweihan21@gmail.com>
2020-12-09 11:54:53 +0800

    save

--- api/models.py
+++ api/models.py
@@ -11,9 +11,7 @@ class Task(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    author =models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
