9a216f4b984c0184acd0b7048c8aab098cfb7fcd
cfssapp <gohweihan21@gmail.com>
2020-12-12 13:28:15 +0800

    save

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api.apps.ApiConfig',
-    'api.apps.BasicListConfig',
+    'basic_list.apps.BasicListConfig',
     
     'rest_framework',
     'corsheaders',
