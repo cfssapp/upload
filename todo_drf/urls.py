eed7a44924083482bc6fe1d24e80dc6ddd12ca5a
cfssapp <gohweihan21@gmail.com>
2020-12-14 18:34:57 +0800

    save

--- todo_drf/urls.py
+++ todo_drf/urls.py
@@ -19,10 +19,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('antd/', include('basic_list.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
