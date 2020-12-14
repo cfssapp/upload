28c01c1352f1f87d6880e1bac6e1ba744dee9dad
cfssapp <gohweihan21@gmail.com>
2020-12-14 18:56:28 +0800

    save

--- basic_list/models.py
+++ basic_list/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class basicList(models.Model):
     title = models.CharField(max_length=100)
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='basic_list', default=1)
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basic_list', default=1)
 
     def __str__(self):
         return self.title
