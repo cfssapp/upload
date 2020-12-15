d466fb699dd1300e1127ecde37c483ba635ea203
cfssapp <gohweihan21@gmail.com>
2020-12-15 10:22:32 +0800

    save

--- basic_list/urls.py
+++ basic_list/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.apiOverview, name="api-overview"),
+	# path('', views.apiOverview, name="api-overview"),
 	path('basic-list/', views.basic_list),
 ]
 
