e8ccaad6b925323ccde564f0c46affcea356e2d0
cfssapp <gohweihan21@gmail.com>
2020-12-16 14:06:21 +0800

    save

--- basic_list/views.py
+++ basic_list/views.py
@@ -51,7 +51,7 @@ class PostUserWritePermission(BasePermission):
 
 class basic_list(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
-    queryset = basicList.postobjects.all()
+    queryset = basicList.objects.all()
     serializer_class = basicListSerializer
 
 
