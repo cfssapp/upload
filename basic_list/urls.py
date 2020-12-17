2d8d3f64ee98ab649a557f505cc3be5bd7ed1317
cfssapp <gohweihan21@gmail.com>
2020-12-17 16:57:58 +0800

    save

--- basic_list/urls.py
+++ basic_list/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.apiOverview, name="api-overview"),
 	path('current-user/', views.currentUser, name="current_user"),
+	path('notices-list', views.notices_list, name="notices_list"),
 
 	path('basic-list/', views.basic_list.as_view(), name='listcreate'),
 	path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
--- basic_list/views.py
+++ basic_list/views.py
@@ -16,6 +16,19 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 def apiOverview(request):
+	api_urls = {}
+	return JsonResponse(api_urls, safe=False)
+
+def currentUser(request):
+	api_urls = {
+    "name": "Serati Ma",
+    "avatar": "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
+    "userid": "00000001",
+    "email": "antdesign@alipay.com",
+}
+	return JsonResponse(api_urls, safe=False)    
+
+def notices_list(request):
 	api_urls = [
   {
       "id": "000000001",
@@ -26,60 +39,6 @@ def apiOverview(request):
   },
 ]
 	return JsonResponse(api_urls, safe=False)
-
-def currentUser(request):
-	api_urls = {
-    "name": "Serati Ma",
-    "avatar": "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
-    "userid": "00000001",
-    "email": "antdesign@alipay.com",
-    "signature": "海纳百川，有容乃大",
-    "title": "交互专家",
-    "group": "蚂蚁集团－某某某事业群－某某平台部－某某技术部－UED",
-    "tags": [
-        {
-            "key": "0",
-            "label": "很有想法的"
-        },
-        {
-            "key": "1",
-            "label": "专注设计"
-        },
-        {
-            "key": "2",
-            "label": "辣~"
-        },
-        {
-            "key": "3",
-            "label": "大长腿"
-        },
-        {
-            "key": "4",
-            "label": "川妹子"
-        },
-        {
-            "key": "5",
-            "label": "海纳百川"
-        }
-    ],
-    "notifyCount": 12,
-    "unreadCount": 11,
-    "country": "China",
-    "geographic": {
-        "province": {
-            "label": "浙江省",
-            "key": "330000"
-        },
-        "city": {
-            "label": "杭州市",
-            "key": "330100"
-        }
-    },
-    "address": "西湖区工专路 77 号",
-    "phone": "0752-268888888"
-}
-	return JsonResponse(api_urls, safe=False)    
-
 
 # def basic_list(request):
      
