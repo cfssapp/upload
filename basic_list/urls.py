c75a90fbf8e2401d1f93153d029f1610c7146c47
cfssapp <gohweihan21@gmail.com>
2020-12-17 16:20:51 +0800

    save

--- basic_list/urls.py
+++ basic_list/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.apiOverview, name="api-overview"),
-	path('currentUser/', views.currentUser, name="currentUser"),
+	path('current-user/', views.currentUser, name="current_user"),
 
 	path('basic-list/', views.basic_list.as_view(), name='listcreate'),
 	path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
