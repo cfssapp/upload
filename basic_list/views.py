0bbef8c4baf87a23e937ac70fb24a7864ab392ee
cfssapp <gohweihan21@gmail.com>
2020-12-17 12:45:00 +0800

    save

--- basic_list/views.py
+++ basic_list/views.py
@@ -17,18 +17,56 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 @csrf_exempt
 @api_view(['GET'])
 def apiOverview(request):
-	api_urls = [{
-        "logo": "https://64.media.tumblr.com/avatar_84bf27cec37f_128.pnj",
-        "title": "Albert Einstein",
-        "href": "https://www.google.com.my/",
-        "subDescription": "Imagination is more important than knowledge",
-        
-        "owner": "User",
-        "createdAt": 1607155074056,
-        "updatedAt": 1607155074056,
-        "percent": 83,
-        "status": "active", 
-    }] 
+	api_urls = {
+    "name": "Serati Ma",
+    "avatar": "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
+    "userid": "00000001",
+    "email": "antdesign@alipay.com",
+    "signature": "海纳百川，有容乃大",
+    "title": "交互专家",
+    "group": "蚂蚁集团－某某某事业群－某某平台部－某某技术部－UED",
+    "tags": [
+        {
+            "key": "0",
+            "label": "很有想法的"
+        },
+        {
+            "key": "1",
+            "label": "专注设计"
+        },
+        {
+            "key": "2",
+            "label": "辣~"
+        },
+        {
+            "key": "3",
+            "label": "大长腿"
+        },
+        {
+            "key": "4",
+            "label": "川妹子"
+        },
+        {
+            "key": "5",
+            "label": "海纳百川"
+        }
+    ],
+    "notifyCount": 12,
+    "unreadCount": 11,
+    "country": "China",
+    "geographic": {
+        "province": {
+            "label": "浙江省",
+            "key": "330000"
+        },
+        "city": {
+            "label": "杭州市",
+            "key": "330100"
+        }
+    },
+    "address": "西湖区工专路 77 号",
+    "phone": "0752-268888888"
+}
 	return JsonResponse(api_urls, safe=False)
 
 
