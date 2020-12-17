25af74a610b690c56fd079a89e47d5ef1260a16d
cfssapp <gohweihan21@gmail.com>
2020-12-17 12:42:27 +0800

    save

--- basic_list/urls.py
+++ basic_list/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.apiOverview.as_view(), name="api-overview"),
+	path('', views.apiOverview, name="api-overview"),
 	path('basic-list/', views.basic_list.as_view(), name='listcreate'),
 	path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
