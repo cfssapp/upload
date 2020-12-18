38fdee9ce0c2f3283426ee540347a86a076b545f
cfssapp <gohweihan21@gmail.com>
2020-12-18 16:35:06 +0800

    save

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -152,7 +152,7 @@ CORS_ORIGIN_WHITELIST = [
 AUTH_USER_MODEL = "users.NewUser"
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
