53c4e9ad6c47e901bc93f6f65edc78c4dd01c43d
cfssapp <gohweihan21@gmail.com>
2020-12-20 19:02:55 +0800

    save

--- basic_list/serializers.py
+++ basic_list/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import basicList
 
 class basicListSerializer(serializers.ModelSerializer):
-    authors = serializers.StringRelatedField(many=True)
+    # authors = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = basicList
