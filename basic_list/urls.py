93f6780c9bbe936c7f4eb81a8487e67a144473b9
cfssapp <gohweihan21@gmail.com>
2020-12-22 14:04:44 +0800

    save

--- basic_list/urls.py
+++ basic_list/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
 	path('', views.apiOverview, name="api-overview"),
-	# path('current-user2/', views.currentUser2, name="current_user2"),
+	
 	path('notices-list/', views.notices_list, name="notices_list"),
 
 	path('todo-list/', views.todo_list.as_view(), name='todolistcreate'),
 	
 
-	
+	# path('current-user2/', views.currentUser2, name="current_user2"),
 	# path('test-list/', views.test_list, name='testlistcreate'),
 ]
 
