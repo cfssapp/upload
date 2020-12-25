e01a597b9d0ca59e606b2b3d0d2460f58271d584
cfssapp <gohweihan21@gmail.com>
2020-12-25 10:00:28 +0800

    save

--- basic_list/urls.py
+++ basic_list/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
 
 	path('todo-list/', views.todo_list.as_view(), name='todolistcreate'),
 
-	path('post/<int:pk>/', PostDetail.as_view(), name='detailcreate'),
-    path('post', PostList.as_view(), name='listcreate'),
+	path('post/<int:pk>/', views.PostDetail.as_view(), name='detailcreate'),
+    path('post', views.PostList.as_view(), name='listcreate'),
 	
 ]
 
