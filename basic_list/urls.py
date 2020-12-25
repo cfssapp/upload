8f83a93d9c0af7897f0c0cdf87af768d3fec90c2
cfssapp <gohweihan21@gmail.com>
2020-12-25 11:34:51 +0800

    save

--- basic_list/urls.py
+++ basic_list/urls.py
@@ -3,17 +3,17 @@ from . import views
 
 from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-	path('', views.apiOverview, name="api-overview"),
+# urlpatterns = [
+# 	path('', views.apiOverview, name="api-overview"),
 	
-	path('notices-list/', views.notices_list, name="notices_list"),
+# 	path('notices-list/', views.notices_list, name="notices_list"),
 
-	path('todo-list/', views.todo_list.as_view(), name='todolistcreate'),
+# 	path('todo-list/', views.todo_list.as_view(), name='todolistcreate'),
 
-	# path('post/<int:pk>/', views.PostDetail.as_view(), name='detailcreate'),
-    # path('post/', views.PostList.as_view(), name='listcreate'),
+# 	# path('post/<int:pk>/', views.PostDetail.as_view(), name='detailcreate'),
+#     # path('post/', views.PostList.as_view(), name='listcreate'),
 	
-]
+# ]
 
 
 app_name = 'basic_list'
