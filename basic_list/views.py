57bdd1516b45d9a70317bf1117e981061595b943
cfssapp <gohweihan21@gmail.com>
2020-12-25 11:29:43 +0800

    save

--- basic_list/views.py
+++ basic_list/views.py
@@ -74,7 +74,7 @@ class PostList(viewsets.ModelViewSet):
 
     def get_object(self, queryset=None, **kwargs):
         item = self.kwargs.get('pk')
-        return get_object_or_404(todoList, slug=item)
+        return get_object_or_404(todoList)
 
     # Define Custom Queryset
     def get_queryset(self):
