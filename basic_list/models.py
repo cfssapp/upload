549002030bc306ea5d2fca7388983fa3a777d37b
cfssapp <gohweihan21@gmail.com>
2020-12-25 16:39:17 +0800

    save

--- basic_list/models.py
+++ basic_list/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.conf import settings
 
-
 class todoList(models.Model):
     title = models.CharField(max_length=100)
     owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='todo_post')
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='todo_post', default=1)
 
     def __str__(self):
         return self.title
