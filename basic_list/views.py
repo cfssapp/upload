cf945d3f5f8c5a55f75dacf8d4b0a45911695d83
cfssapp <gohweihan21@gmail.com>
2020-12-26 12:11:34 +0800

    save

--- basic_list/views.py
+++ basic_list/views.py
@@ -119,6 +119,22 @@ class EditPost(generics.UpdateAPIView):
     serializer_class = todoListSerializer
     queryset = todoList.objects.all()
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        articles = todoList.objects.filter(owner=self.request.user)
+        serializer = todoListSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
 class DeletePost(generics.RetrieveDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = todoListSerializer
