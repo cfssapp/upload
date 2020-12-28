8ce5be5cde37173943c25f41d401c6c4125589e0
cfssapp <gohweihan21@gmail.com>
2020-12-28 21:17:20 +0800

    save

--- wp_parcel/serializers.py
+++ wp_parcel/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import todoList
+from .models import parcelList
 
 class parcelListSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = todoList
+        model = parcelList
         # fields = ['id', 'title', 'owner_id']
         fields ='__all__'
 
