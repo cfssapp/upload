87dbe8d41a4f3acf5a11ad392bafaf858a0a08ad
cfssapp <gohweihan21@gmail.com>
2020-12-29 10:01:00 +0800

    save

--- wp_parcel/urls.py
+++ wp_parcel/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ParcelList.as_view(), name='listcreate'),
-	path('<int:pk>/', views.ParcelDetail.as_view(), name='detailcreate'),
-    path('create/', views.CreateParcel.as_view(), name='createpost'),
-    path('edit/<int:pk>/', views.EditParcel.as_view(), name='editpost'),
-    path('delete/<int:pk>/', views.DeleteParcel.as_view(), name='deletepost'),
+    path('', views.ParcelList.as_view(), name='listparcel'),
+	path('<int:pk>/', views.ParcelDetail.as_view(), name='detailparcel'),
+    path('create/', views.CreateParcel.as_view(), name='createparcel'),
+    path('edit/<int:pk>/', views.EditParcel.as_view(), name='editparcel'),
+    path('delete/<int:pk>/', views.DeleteParcel.as_view(), name='deleteparcel'),
 ]
