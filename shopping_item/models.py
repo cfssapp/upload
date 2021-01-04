fef944950c2d373ce7a8048ea8b999c63b51b60a
cfssapp <gohweihan21@gmail.com>
2021-01-04 20:43:05 +0800

    save

--- shopping_item/models.py
+++ shopping_item/models.py
@@ -2,47 +2,14 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-CATEGORY_CHOICES = (
-    ('S', 'Shirt'),
-    ('SW', 'Sport wear'),
-    ('OW', 'Outwear')
-)
-
-LABEL_CHOICES = (
-    ('P', 'primary'),
-    ('S', 'secondary'),
-    ('D', 'danger')
-)
-
-ADDRESS_CHOICES = (
-    ('B', 'Billing'),
-    ('S', 'Shipping'),
-)
-
 class Item(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.FloatField()
-    discount_price = models.FloatField(blank=True, null=True)
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
-    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
-    slug = models.SlugField()
-    description = models.TextField()
-    image = models.ImageField()
+    tracking_no = models.CharField(max_length=100)
+    weight = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    remark = models.CharField(max_length=100)
+    parcel_owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='parcel_list', default=1)
 
     def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse("core:product", kwargs={
-            'slug': self.slug
-        })
-
-    def get_add_to_cart_url(self):
-        return reverse("core:add-to-cart", kwargs={
-            'slug': self.slug
-        })
-
-    def get_remove_from_cart_url(self):
-        return reverse("core:remove-from-cart", kwargs={
-            'slug': self.slug
-        })
+        return self.tracking_no
