c09bb622f8561612dd4231458cb0352580c34a07
cfssapp <gohweihan21@gmail.com>
2021-01-05 10:51:27 +0800

    save

--- shop_item/models.py
+++ shop_item/models.py
@@ -13,3 +13,22 @@ class Item(models.Model):
 
     def __str__(self):
         return self.tracking_no
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.username
