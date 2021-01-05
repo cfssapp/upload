c87954950e48af6a1e3abfb59e0fd01737e24d11
cfssapp <gohweihan21@gmail.com>
2021-01-05 09:07:32 +0800

    save

--- shop_item/urls.py
+++ shop_item/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ItemList.as_view(), name='listitem'),
-	path('<int:pk>/', views.ItemDetail.as_view(), name='detailitem'),
-    path('create/', views.CreateItem.as_view(), name='itemcreate'),
-    path('edit/<int:pk>/', views.EditItem.as_view(), name='itemedit'),
-    path('delete/<int:pk>/', views.DeleteItem.as_view(), name='itemdelete'),
+    path('item/', views.ItemList.as_view(), name='listitem'),
+	path('item/<int:pk>/', views.ItemDetail.as_view(), name='detailitem'),
+    path('item/create/', views.CreateItem.as_view(), name='itemcreate'),
+    path('item/edit/<int:pk>/', views.EditItem.as_view(), name='itemedit'),
+    path('item/delete/<int:pk>/', views.DeleteItem.as_view(), name='itemdelete'),
 ]
