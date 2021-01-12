bdf4cba40cf1a078469c063fbd754994179abe4c
cfssapp <gohweihan21@gmail.com>
2021-01-12 11:34:31 +0800

    save

--- shop_item/models.py
+++ shop_item/models.py
@@ -11,8 +11,8 @@ class Item(models.Model):
     category = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
     remark = models.CharField(max_length=100)
-    item_owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='item', default=1)
+    # item_owner = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='item', default=1)
     ordered = models.BooleanField(default=False)
     cartadded = models.BooleanField(default=False)
     # order_id = models.ForeignKey('Order', on_delete=models.CASCADE, default=29)
@@ -22,16 +22,16 @@ class Item(models.Model):
 
 
 class OrderItem(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orderitem', default=1)
+    # user = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orderitem', default=1)
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     
 
 class Order(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='order', default=1)
+    # user = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='order', default=1)
     items = models.ManyToManyField(Item)
     shipping_address = models.CharField(max_length=100, default="not set")
     
